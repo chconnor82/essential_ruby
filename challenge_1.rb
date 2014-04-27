48d8037ec7d03aaf9c3c2d85acc70111cdf82e0f
Charlie Connor <chconnor@gmail.com>
2014-04-26 19:49:04 -0500

    Challenge_1
    Updated with new variables

--- challenge_1.rb
+++ challenge_1.rb
@@ -4,21 +4,16 @@
 
 # Suppose we are building a loan payment calculator.
 
-# Define a method called "pmt" which will accept three arguments:
-#  - interest rate
-#  - number of payments
-#  - principal value of loan
 # The method should return the size of each payment given these three things.
 # Before we can teach the computer how to automate a task,
 #   we usually have to do some research on how the task is done:
 #   http://en.wikipedia.org/wiki/Mortgage_calculator
 
-def pmt(rate, nper, pv)
-  # ============================================
-  # Your code to implement the method goes here.
-  # ============================================
+def loan_payment(interest_rate, number_of_years, principal_value_of_loan)
+  monthly_interest_rate = (interest_rate / 100 / 12)
+  ((monthly_interest_rate * principal_value_of_loan) / (1 - (1 + monthly_interest_rate) ** (-number_of_years * 12))).round(2)
 end
 
-# Example usage of the method:
+monthly_payment = loan_payment(6.5, 30, 200000)
 
-# puts "Your monthly payment will be #{pmt(0.01, 60, 30000)}."
+puts "The return value is #{monthly_payment}"
